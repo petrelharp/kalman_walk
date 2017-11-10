b5739e01bb6a309315a61c2d1c28ce94b9abbeb8
peter <petrel.harp@gmail.com>
2017-11-10 12:09:03 -0800

    added tests and refactored

--- sims/perturb_Kalman.r
+++ sims/perturb_Kalman.r
@@ -4,119 +4,124 @@ library(expm)
 
 sigma <- 1
 #####################################
-h <- function (t,M) { sapply(t, function (tt) CK %*% expm(tt*M) %*% BK) }
+h <- function (t, M, BK, CK) { sapply(t, function (tt) CK %*% expm(tt*M) %*% BK) }
 optimal_h <- function (t) { sin(t)+cos(t) }
 ###################################
-Df <- function (A,t) {
-  exp(-t/(4*pi)) * ( h(t,M=A) - optimal_h(t) )^2
+Df <- function (A,t, BK, CK) {
+  exp(-t/(4*pi)) * ( h(t, M=A, BK=BK, CK=CK) - optimal_h(t) )^2
 }
 ####################################
-D <- function (A, upper=10, ...) {
-  f <- function (t) { Df(A,t) }
+D <- function (A, BK, CK, upper=10, ...) {
+  f <- function (t) { Df(A,t, BK, CK) }
   integrate(f, lower=0, upper=upper, ...)$value
 }
 
 ##################################
-recombine <- function (X,Y) 
+recombine<- function (X,Y) 
 {
   n <- dim(X)[1];
-  G <- matrix(0,nrow=n,ncol=n);
   r <- matrix(c(rbinom(n^2,1,0.5)),nrow=n);
-  for (i in 1:n) 
-  {
-    for (j in 1:n)
-    {
-      if (r[i,j]==0)
-      {
-        G[i,j] = X[i,j]
-      }
-      else if (r[i,j] == 1)
-      {
-        G[i,j] = Y[i,j]
-      }
-    }
-  }
-  G
+  G <- X * r + Y * (1-r)
+  return(G)
 }
-#####################################
-sys_gen2 <- function (s=sigma) 
-{
-  A <- matrix(c(0,-1,1,0), nrow=2);
-  B <- matrix(c(1,1), ncol=1);
-  C <- matrix(c(1,0), nrow=1);
-  A00 <- matrix(c(0,0,0,0), nrow=2);
-  B00 <- matrix(c(0,0), ncol=1);
-  C00 <- matrix(c(0,0), nrow=1);
-  #A14 <- matrix(c(rnorm(4,mean=0, sd=1)),nrow=2);
-  A12 <- matrix(c(rnorm(4,mean=0, sd=s)),nrow=2);
-  Arno <- matrix(c(rnorm(4,mean=0, sd=s)),nrow=2);
-  A13 <- matrix(c(rnorm(4,mean=0, sd=s)),nrow=2);
-  #A24 <- matrix(c(rnorm(4,mean=0, sd=1)),nrow=2);
-  Anrno <- matrix(c(rnorm(4,mean=0, sd=s)),nrow=2);
-  
-  p <- rnorm(25,mean=0, sd=s);
+
+make_P <- function (p) {
+  # Make the matrix P from a vector of entries
+  stopifnot(length(p)==25)
   P <- matrix(c(1,1-p[1],1-p[2],p[3],p[4],p[5],
                 0,p[1],p[2],-p[3],-p[4],-p[5],
                 0,0,p[7],p[8],p[9],p[10],
                 0,0,p[12],p[13],p[14],p[15],
                 0,0,p[17],p[18],p[19],p[20],
                 0,0,p[22],p[23],p[24],p[25]), nrow=6);
-  invP <- solve(P);
+  return(P)
+}
+
+# Generate a starting matrix by adding noise 
+# to the free parameters of the Kalman decomposition
+# for the oscillator.
+random_oscillator <- function(s) {
+  A <- matrix(c(0,-1,1,0), nrow=2);
+  B <- matrix(c(1,1), ncol=1);
+  C <- matrix(c(1,0), nrow=1);
+  #A14 <- matrix(c(rnorm(4,mean=0, sd=1)),nrow=2);
+  A12 <- matrix(c(rnorm(4,mean=0, sd=s)),nrow=2);
+  Arno <- matrix(c(rnorm(4,mean=0, sd=s)),nrow=2);
+  A13 <- matrix(c(rnorm(4,mean=0, sd=s)),nrow=2);
+  #A24 <- matrix(c(rnorm(4,mean=0, sd=1)),nrow=2);
+  Anrno <- matrix(c(rnorm(4,mean=0, sd=s)),nrow=2);
+  p <- rnorm(25,mean=0, sd=s);
+  P <- make_P(p)
+  return(list(A=A, A12=A12, Arno=Arno, Anrno=Anrno, A13=A13, B=B, C=C, P=P))
+}
+
+# assemble the Kalman system from a list of components
+assemble_oscillator <- function (AL) {
+    # AL should be a list with components:
+    # A
+    # A12
+    # Arno
+    # Anrno
+    # A13
+    # B
+    # C
+    # P
+  A00 <- matrix(c(0,0,0,0), nrow=2);
+  B00 <- matrix(c(0,0), ncol=1);
+  C00 <- matrix(c(0,0), nrow=1);
+  AK <- (rbind(cbind(AL$A, A00, A00),
+               cbind(AL$A12, AL$Arno, AL$A13),
+               cbind(A00, A00, AL$Anrno)));
+  # pad B and C with zeros
+  BK <- matrix(c(AL$B, rep(0, 4)), ncol=1)
+  CK <- matrix(c(AL$C, rep(0, 4)), nrow=1)
+  invP <- solve(AL$P);
+  KalC <- CK%*%AL$P;
+  KalB <- AL$P%*%BK;
+  KalA <- AL$P%*%AK%*%invP
+  return(list(A=KalA, B=BK, C=CK))
+}
+
+# Perturb an oscillator of the form above
+perturb_oscillator <- function(AL, eps) {
+    mA12 <- matrix(c(rnorm(4, mean=0, sd=eps)),nrow=2,ncol=2)
+    mArno <- matrix(c(rnorm(4, mean=0, sd=eps)),nrow=2,ncol=2)
+    mA13 <- matrix(c(rnorm(4, mean=0, sd=eps)),nrow=2,ncol=2)
+    mAnrno <- matrix(c(rnorm(4, mean=0, sd=eps)),nrow=2,ncol=2)
+    mp <- rnorm(25,mean=0,sd=eps)
+    
+    new_A12 <- AL$A12 + AL$A12*mA12
+    new_Arno <- AL$Arno + AL$Arno*mArno
+    new_A13 <- AL$A13 + AL$A13*mA13
+    new_Anrno <- AL$Anrno + AL$Anrno*mAnrno
+    mP <- AL$P + make_P(mp)
+    return(list(A=AL$A, A12=new_A12, Arno=new_Arno, Anrno=new_Anrno, 
+                A13=new_A13, B=AL$B, C=AL$C, P=mP))
+}
+
+#####################################
+sys_gen2 <- function (s=sigma, eps=0.1, nreps=100) 
+{
+  KalA_list <- random_oscillator(s=s)
+  KalA <- assemble_oscillator(KalA_list)
   
-  AK <- (rbind(cbind(A,A00,A00),
-               cbind(A12,Arno,A13),
-               cbind(A00,A00, Anrno)));
-  BK <- matrix(c(1,1,0,0,0,0),ncol=1);
-  CK <- matrix(c(1,0,0,0,0,0),nrow=1);
-  KalC <- CK%*%P;
-  KalB <- P%*%BK;
-  KalA <- P%*%AK%*%invP
-  
-  output <- matrix(0,nrow=3000,ncol=2)
-  for (i in 1:3000)
+  output <- matrix(0,nrow=nreps,ncol=2)
+  colnames(output) <- c("sys_dist", "pheno_dist")
+  for (i in 1:nreps)
   {
-    nam0 <- paste("new_KalA", i, sep = ""); 
     
-    mA12 <- matrix(c(rnorm(4, mean=0, sd=0.1)),nrow=2,ncol=2)
-    mArno <- matrix(c(rnorm(4, mean=0, sd=0.1)),nrow=2,ncol=2)
-    mA13 <- matrix(c(rnorm(4, mean=0, sd=0.1)),nrow=2,ncol=2)
-    mAnrno <- matrix(c(rnorm(4, mean=0, sd=0.1)),nrow=2,ncol=2)
-    mp <- rnorm(25,mean=0,sd=0.1)
-    
-    new_A12 <- A12 + A12*mA12
-    new_Arno <- Arno + Arno*mArno
-    new_A13 <- A13 + A13*mA13
-    new_Anrno <- Anrno + Anrno*mAnrno
-    new_p <- p + p*mp
-    mP <- matrix(c(1,1-new_p[1],1-new_p[2],new_p[3],new_p[4],new_p[5],
-                   0,new_p[1],new_p[2],-new_p[3],-new_p[4],-new_p[5],
-                   0,0,new_p[7],new_p[8],new_p[9],new_p[10],
-                   0,0,new_p[12],new_p[13],new_p[14],new_p[15],
-                   0,0,new_p[17],new_p[18],new_p[19],new_p[20],
-                   0,0,new_p[22],new_p[23],new_p[24],new_p[25]), nrow=6);
-    invmP <- solve(mP);
-    new_AK <- (rbind(cbind(A,A00,A00),
-                     cbind(new_A12,new_Arno,new_A13),
-                     cbind(A00,A00, new_Anrno)))
-    temp_KalA <- mP%*%new_AK%*%invmP
-    
-    assign(nam0, temp_KalA)
-    
-    dist <- norm(KalA-get(nam0),"F")
+    new_KalA_list <- perturb_oscillator(KalA_list, eps=eps)
+    new_KalA <- assemble_oscillator(new_KalA_list)
+    dist <- norm(KalA$A-new_KalA$A,"F")
     
     avg_div <- 0
     for(j in 1:200) 
     { 
-      #nam <- paste("Z", j, sep = "") 
-      #assign(nam, recombine(KalA,get(nam0)))
-      Z1 <- recombine(KalA, get(nam0))
-      Z2 <- recombine(KalA, get(nam0))
+      Z1 <- recombine(KalA$A, new_KalA$A)
+      Z2 <- recombine(KalA$A, new_KalA$A)
       Z3 <- (Z1 + Z2)/2
-      #nam2 <- paste("DZ", j, sep = "")
-      #assign(nam2, D(get(nam)))
-      DZ <- D(Z3)
+      DZ <- D(Z3, BK=KalA$B, CK=KalA$C)
       
-      #avg_div <- avg_div + get(nam2)/100
       avg_div <- avg_div + DZ/200
     }
     output[i,] <- c(dist,avg_div)
---
+++ sims/test_perturb_Kalman.r
@@ -0,0 +1,52 @@
+source("perturb_Kalman.r")
+
+set.seed(23)
+
+check_system <- function (sys) {
+    obs_D <- abs(D(sys$A, sys$B, sys$C))
+    if (obs_D > 1e-7) {
+        cat("Error: D=", obs_D, "\n")
+        return(1)
+    }
+    return(0)
+}
+
+# check that D(optimal) = 0
+optimal_system <- list(
+  A = matrix(c(0,-1,1,0), nrow=2),
+  B = matrix(c(1,1), ncol=1),
+  C = matrix(c(1,0), nrow=1) )
+check_system(optimal_system)
+
+num_errors <- 0
+for (sigma in c(0.001, 0.1, 1.0)) {
+    for (eps in c(0.001, 0.1, 1.0)) {
+        cat(sprintf("Testing with sigma=%f and eps=%f\n", sigma, eps))
+        AL <- random_oscillator(sigma)
+        random_system <- assemble_oscillator(AL)
+
+        cat("check that random_oscillator returns an oscillator\n")
+        num_errors <- num_errors + check_system(random_system)
+
+        cat("check that random_oscillator does not change B and C\n")
+        num_errors <- num_errors + !(all(abs(random_system$B - c(1,1,rep(0,length(random_system$B)-2))) < 1e-16))
+        num_errors <- num_errors + !(all(abs(random_system$C - c(1,0,rep(0,length(random_system$B)-2))) < 1e-16))
+
+        pAL <- perturb_oscillator(AL, eps=eps)
+        perturbed_system <- assemble_oscillator(pAL)
+        num_errors <- num_errors + !(all(sapply(AL, dim) == sapply(pAL, dim)))
+
+        cat("check that perturb_oscillator returns an oscillator\n")
+        num_errors <- num_errors + check_system(perturbed_system)
+
+        cat("check that perturb_oscillator does not change B and C\n")
+        num_errors <- num_errors + !(all(abs(perturbed_system$B - c(1,1,rep(0,length(perturbed_system$B)-2))) < 1e-16))
+        num_errors <- num_errors + !(all(abs(perturbed_system$C - c(1,0,rep(0,length(perturbed_system$B)-2))) < 1e-16))
+    }
+}
+
+if (num_errors>0) {
+    stop("Failed with ", num_errors, " errors.")
+} else {
+    cat("All good.\n")
+}
