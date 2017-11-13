e33cd02a5791281432c3e06e69ade7e41b0aa26d
peter <petrel.harp@gmail.com>
2017-11-13 13:04:30 -0800

    another row to speciation rates fig

--- speciation_rates.R
+++ speciation_rates.R
@@ -14,12 +14,14 @@ f1 <- function (t, a, b, c_mu=1/20) {
     exp(-(c_mu * t)^2 / (1 + b + 4 * b * t))
 }
 
-plotf <- function (main, ...) {
-    tt <- seq(0, 10, length.out=200)
-    plot(tt, f(tt, ...), type='l', ylim=c(0,1), main=main,
-            xlab=expression(T/N[e]), ylab="F2 fitness")
-    lines(tt, f1(tt, ...), col='red', lty=3)
-    lines(tt, f2(tt, ...), col='green', lty=3)
+plotf <- function (main, Ne=1.0, tmax=10,
+        ...) {
+    tt <- seq(0, tmax/Ne, length.out=200)
+    plot(tt*Ne, f(tt, ...), type='l', ylim=c(0,1), main=main,
+            xlab=if (Ne==1) { expression(T/N[e])} else { expression(T) }, 
+            ylab="F2 fitness")
+    lines(tt*Ne, f1(tt, ...), col='red', lty=3)
+    lines(tt*Ne, f2(tt, ...), col='green', lty=3)
 }
 
 pdf(file="speciation_rates.pdf", width=7, height=3, pointsize=10)
@@ -28,6 +30,25 @@ layout(t(1:3))
 plotf(a=1, b=1, main="large population")
 plotf(a=.1, b=.1, main="small population")
 plotf(a=10, b=1, main="metapopulation")
+
+legend("topright", lty=c(1,3,3), col=c('black', 'red', 'green'),
+        legend=c("total fitness", "mean", "variance"))
+
+dev.off()
+
+
+
+pdf(file="speciation_rates_2.pdf", width=7, height=6, pointsize=10)
+
+layout(matrix(1:6, nrow=2))
+plotf(a=1, b=1, main="large population")
+plotf(a=1, b=1, main="large population, Ne=1e5", Ne=1e5, tmax=1e4)
+
+plotf(a=.1, b=.1, main="small population")
+plotf(a=.1, b=.1, main="small population, Ne=1e3", Ne=1e3, tmax=1e4)
+
+plotf(a=10, b=1, main="metapopulation")
+plotf(a=10, b=1, main="metapopulation, Ne=1e7", Ne=1e7, tmax=1e4)
 
 legend("topright", lty=c(1,3,3), col=c('black', 'red', 'green'),
         legend=c("total fitness", "mean", "variance"))
