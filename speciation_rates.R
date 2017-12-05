07a967f74adf36807863b5ef496265ee165ef879
peter <petrel.harp@gmail.com>
2017-12-05 12:31:55 -0800

    edited through

--- speciation_rates.R
+++ speciation_rates.R
@@ -14,14 +14,14 @@ f1 <- function (t, a, b, c_mu=1/20) {
     exp(-(c_mu * t)^2 / (1 + b + 4 * b * t))
 }
 
-plotf <- function (main, Ne=1.0, tmax=10,
+plotf <- function (main, Ne=1.0, tmax=5,
         ...) {
     tt <- seq(0, tmax/Ne, length.out=200)
     plot(tt*Ne, f(tt, ...), type='l', ylim=c(0,1), main=main,
             xlab=if (Ne==1) { expression(T/N[e])} else { expression(T) }, 
             ylab="F2 fitness")
     lines(tt*Ne, f1(tt, ...), col='red', lty=3)
-    lines(tt*Ne, f2(tt, ...), col='green', lty=3)
+    # lines(tt*Ne, f2(tt, ...), col='green', lty=3)
 }
 
 pdf(file="speciation_rates.pdf", width=7, height=3, pointsize=10)
@@ -31,8 +31,11 @@ plotf(a=1, b=1, main="large population")
 plotf(a=.1, b=.1, main="small population")
 plotf(a=10, b=1, main="metapopulation")
 
-legend("topright", lty=c(1,3,3), col=c('black', 'red', 'green'),
-        legend=c("total fitness", "shift of mean", "increase of variance"))
+legend("topright", lty=c(1,3), col=c('black', 'red', 'green'), bg='white',
+        legend=c("F2 fitness", "F1 fitness"))
+
+# legend("topright", lty=c(1,3,3), col=c('black', 'red', 'green'),
+#         legend=c("total fitness", "shift of mean", "increase of variance"))
 
 dev.off()
 
